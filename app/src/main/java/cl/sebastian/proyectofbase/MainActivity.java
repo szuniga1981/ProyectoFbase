ba8a93910bff28aab46823fec5e82f5b90edb97b
sebastian <szuniga1981@gmail.com>
2020-12-03 13:59:44 -0300

    -crear proyecto Fbase, se crea en firebase web, se agrega metodo

---
+++ app/src/main/java/cl/sebastian/proyectofbase/MainActivity.java
@@ -0,0 +1,59 @@
+package cl.sebastian.proyectofbase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final Object TAG = "Main Activity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicializarFbase();// crear metodo inicializarFbase.
+
+    }
+        // activar "add cloud Firestore to you app.
+        // se copia y se pega  el "initialize cloud firestore
+    private void inicializarFbase() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        //Add data (copiar datos de firebase)
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("first", "Ada");
+        user.put("last", "Lovelace");
+        user.put("born", 1815);
+
+// Add a new document with a generated ID
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d((String) TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w((String) TAG, "Error adding document", e);
+                    }
+                });
+
+    }
+
+
+}
